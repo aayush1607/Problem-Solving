fa53b683281d06c3eb64d004a22369caee307d9a
aayush1607 <auc1607@gmail.com>
2020-06-27 20:36:07 +0530

    add

---
+++ BasicConcepts/Factorial2/Factorial_of_large_2.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 200
+
+string factorial(long long n)
+{
+
+    if(n>MAX)
+    {
+        cout<<"OverFlow";
+        return "";
+    }
+    long long i;
+    long double sum=0;
+
+    if(n==0){
+        return"1";
+    }
+
+    for(i=1;i<=n;i++)
+    {
+        sum+=log(i);
+    }
+
+    string result= to_string(round(exp(sum)));
+    return result;
+
+}
+
+int main()
+{
+    cout<<factorial(100)<<"\n";
+}
